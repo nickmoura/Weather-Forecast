3edbbb8c667d99907b8ef8fd897b9d9516d9b328
Nick Moura <120116187+nickmoura@users.noreply.github.com>
2024-04-06 18:44:03 -0300

    API added Co-authored-by: Victor Viana <nosferavic@users.noreply.github.com> Co-authored-by: Matheus Tiburcio <tiburci0@users.noreply.github.com> Co-authored-by: Carla Zola <CarlaZola@users.noreply.github.com> Co-authored-by: maxalexandre15 <maxalexandre15@users.noreply.github.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,32 @@
-const apiKey = "e71afb0d9c51bbc204fe70de6f0dd8ad"
-const url = `https://api.openweathermap.org/data/3.0/onecall?appid=${apiKey}`
+const form = document.querySelector('.form')
+const temp = document.querySelector('.temperatura')
+const clima = document.querySelector('.clima')
+const umidade = document.querySelector('.umidade')
+const velocidadeVento = document.querySelector('.velocidadeVento')
+const visibilidade = document.querySelector('.visibilidade')
 
-async function getApi(){
+form.addEventListener("submit", async (e) => {
+    e.preventDefault()
+
+    let input = document.querySelector('.cidade')
+
+   await getAPI(input.value)
+})
+
+
+async function getAPI(city){
+    const apiKey = "11ecabe574fe4ddb564aa3bf3d75d241"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data);
+    console.log(data)
 }
 
-getApi()
+getAPI("São Paulo")
+
+
+async function getData(){
+    const iconTemp = `http://openweathermap.org/img/wn/`
+}
+
